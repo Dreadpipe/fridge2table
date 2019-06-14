7898970d0544481132fbfeb6ac5110bc052bf4e3
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-06-13 19:53:45 -0500

    Set up /src/util/API.js file to work with a gitignored env.js file on the front end, for testing

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import env from '../../env';
 
 export default {
   get() {
-    return axios.get('/');
+    return axios.get(`http://${env.IP_ADDRESS}:3001/`);
   },
 };
