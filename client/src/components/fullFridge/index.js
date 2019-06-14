d278efb998ac89e7e94d1b7c64a46311b91334a6
Robert H <robertjameshardin@aol.com>
2019-06-14 13:36:05 -0500

    adjusted spacing by adding additional view component, working on buttons next

--- client/src/components/fullFridge/index.js
+++ client/src/components/fullFridge/index.js
@@ -24,12 +24,16 @@ function FullFridge(props) {
   return (
     <ImageBackground style={styles.background} source={require("./fullFridge_smaller.jpg")}>
       <View style={styles.container}>
-        <Text>Fridge2Table!</Text>      
+        <Text>Fridge</Text>      
       </View>
       <View style={styles.container}>
+        <Text>2</Text>
         <Image style={styles.icon} source={{uri: "https://image.flaticon.com/teams/slug/google.jpg"}} />
         <Button color="red" title="Login with Google" onPress={props.handlePress} />
         <Button title="Login with other..." onPress={props.handlePress} />
+      </View>
+      <View style={styles.container}>
+        <Text>Table</Text>
       </View>
     </ImageBackground>
   );
