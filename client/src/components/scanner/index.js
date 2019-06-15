965666872bd149616b569f8bbd4b8098c9cd7a6a
Robert H <robertjameshardin@aol.com>
2019-06-15 17:01:13 -0500

    added initial scanner component, weeding out OAuth for testing purposes

---
+++ client/src/components/scanner/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {Text, View, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner} from "expo";
+// import { BarCodeScanner } from "expo-barcode-scanner";
+
+styles = create.StyleSheet({
+  scanner: {
+    flex: "1",
+    justifyContent: "flex-end"
+  }
+})
+
+export default class BarCodeScannerExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    scanned: false,
+  };
+
+  async componentDidMount() {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
+  };
+
+  render() {
+    const {hasCameraPermission, scanned} = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permissions</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    return (
+      <View style={styles.scanner}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+        {scanned && (
+          <Button title={'Tap to Scan Again'} onPress={() => this.setState({scanned: false})} />
+        )}
+      </View>
+    );
+    
+    handleBarCodeScanned = ({type, data}) => {
+      this.setState({scanned: true});
+      alert('Bar code with type ${type} and data ${data} has been scanned');
+    }
+  }
+}
