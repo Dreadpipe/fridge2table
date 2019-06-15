490bf062b011c93f7736f83eafd5cbe061af9e17
Robert H <robertjameshardin@aol.com>
2019-06-15 17:24:22 -0500

    added .env variables to satisfy passport's bloodlust, working on scanner component still

--- client/src/components/scanner/index.js
+++ client/src/components/scanner/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import {Text, View, StyleSheet, Button } from "react-native";
 import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
-import { BarCodeScanner} from "expo";
-// import { BarCodeScanner } from "expo-barcode-scanner";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 styles = create.StyleSheet({
   scanner: {
@@ -43,11 +42,11 @@ export default class BarCodeScannerExample extends React.Component {
           <Button title={'Tap to Scan Again'} onPress={() => this.setState({scanned: false})} />
         )}
       </View>
-    );
+    )
+  }
     
     handleBarCodeScanned = ({type, data}) => {
       this.setState({scanned: true});
       alert('Bar code with type ${type} and data ${data} has been scanned');
     }
   }
-}
