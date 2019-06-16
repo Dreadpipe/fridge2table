10464fd02264469b601efb67f9c6ff539e809b88
Druidan <44009123+Druidan@users.noreply.github.com>
2019-06-15 19:42:03 -0500

    Initial tweaks made to models and database routes.

--- models/User.js
+++ models/User.js
@@ -19,14 +19,34 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  products: [
+  allProducts: [ // An array of every barcode ever scanned into the inventory.
     {
-      // Store ObjectIds in the array
-      type: Schema.Types.ObjectId,
-      // The ObjectIds will refer to the ids in the Comment model
-      ref: 'product',
+      type: Number,
+      ref: 'allProduct',
     },
   ],
+  inventoryProducts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'currentInventory',
+    },
+  ],
+  expiredFood: 
+  // [ //If we expand the data we keep on expired foods, we can uncomment the needed bits here.
+    {
+//     type: Schema.Types.ObjectId,
+      type: Number,
+      ref: 'expiredProduct',
+    },
+  // ]
+  lastLogin: {
+    type: Date,
+    required: true,
+  },
+  dateJoined: {
+    type: Date,
+    required: true,
+  },
 });
 
 const User = mongoose.model('user', userSchema);
---
+++ routes/dbRoutes.js
@@ -0,0 +1,31 @@
+// Establish our Express Dependancy
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+// Bring in the database models
+const db = require('../models');
+
+//-------------------------------------
+
+// Create new User
+router.post('/newUser', function (req, res) {
+
+});
+
+//-------------------------------------
+
+//Create new Product
+router.post('/newProduct', function (req, res) {
+
+});
+
+//-------------------------------------
+
+//Update Product Quantity
+
+//-------------------------------------
+
+//Remove Product
+
+//-------------------------------------
