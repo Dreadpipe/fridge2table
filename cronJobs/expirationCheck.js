49e1e0f94e7fdcd8c6a8eb660b2703fd13bb966c
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-06-25 23:38:03 -0500

    Removed commented-out code that got removed on back end

--- cronJobs/expirationCheck.js
+++ cronJobs/expirationCheck.js
@@ -37,63 +37,8 @@ const dailyCheck = function() {
 							);
 							//console.log(err);
 						});
-					// axios
-					// 	.get(
-					// 		`http://${process.env.IP_ADDRESS}:3001/findProductOwner/${
-					// 			product.owner
-					// 		}`
-					// 	)
-					// 	.then(response => {
-					// 		const pushObj = {
-					// 			productname: product.productname,
-					// 			pushToken: response.data[0].pushToken,
-					// 			message: "One of your items is expiring within the week."
-					// 		};
-					// 		SendPushNote(pushObj)
-					// axios
-					// 	.post(
-					// 		`http://${process.env.IP_ADDRESS}:3001/users/push-token`,
-					// 		pushObj
-					// 	)
-					// 	.then(response => {
-					// 		//console.log(response)
-					// 	})
-					// 	.catch(err => {
-					// 		console.log(
-					// 			"We ran into a problem sending the seven day warning to the push notification.\n------------------------"
-					// 		);
-					// 		// console.log(err);
-					// 	});
-					// })
-					// .catch(err => {
-					// 	console.log(
-					// 		"We ran into a problem finding a user to send the seven day warning to.\n------------------------"
-					// 	);
-					// 	// console.log(err);
-					// });
 
 					updateProduct({ _id: product._id }, { remove7DayWarning: true });
-					// const query = {
-					// 	target: {
-					// 		_id: product._id
-					// 	},
-					// 	update: {
-					// 		remove7DayWarning: true
-					// 	}
-					// };
-					// console.log("HERE!!!!! HERE!!!! HERE!!!!!");
-					// console.log(query);
-					// axios
-					// 	.put(`http://${process.env.IP_ADDRESS}:3001/updateProduct`, query)
-					// 	.then(response => {
-					// 		// console.log(response.data)
-					// 	})
-					// 	.catch(err => {
-					// 		console.log(
-					// 			"We ran into a problem removing the seven day warning.\n------------------------"
-					// 		);
-					// 		// console.log(err);
-					// 	});
 				} else if (
 					product.sevenDayWarning === null &&
 					product.twoDayWarning !== null &&
@@ -118,53 +63,8 @@ const dailyCheck = function() {
 							);
 							//console.log(err);
 						});
-					// axios
-					// 	.get(
-					// 		`http://${process.env.IP_ADDRESS}:3001/findProductOwner/${
-					// 			product.owner
-					// 		}`
-					// 	)
-					// 	.then(response => {
-					// 		const pushObj = {
-					// 			productname: product.productname,
-					// 			pushToken: response.data[0].pushToken,
-					// 			message: "One of your items is expiring within two days."
-					// 		};
-					// 		axios
-					// 			.post(
-					// 				`http://${process.env.IP_ADDRESS}:3001/users/push-token`,
-					// 				pushObj
-					// 			)
-					// 			.then(response => {
-					// 				//console.log(response)
-					// 			})
-					// 			.catch(err => {
-					// 				console.log(
-					// 					"We ran into a problem sending the two day warning to the push notification.\n------------------------"
-					// 				);
-					// 				// console.log(err);
-					// 			});
-					// 	});
+
 					updateProduct({ _id: product._id }, { remove2DayWarning: true });
-					// const query = {
-					// 	target: {
-					// 		_id: product._id
-					// 	},
-					// 	update: {
-					// 		remove2DayWarning: true
-					// 	}
-					// };
-					// axios
-					// 	.put(`http://${process.env.IP_ADDRESS}:3001/updateProduct`, query)
-					// 	.then(response => {
-					// 		//console.log(response)
-					// 	})
-					// 	.catch(err => {
-					// 		console.log(
-					// 			"We ran into a problem removing the two day warning.\n------------------------"
-					// 		);
-					// 		// console.log(err);
-					// 	});
 				} else if (
 					product.sevenDayWarning === null &&
 					product.twoDayWarning === null &&
@@ -191,73 +91,10 @@ const dailyCheck = function() {
 							);
 							//console.log(err);
 						});
-					// axios
-					// 	.get(
-					// 		`http://${process.env.IP_ADDRESS}:3001/findProductOwner/${
-					// 			product.owner
-					// 		}`
-					// 	)
-					// 	.then(response => {
-					// 		const pushObj = {
-					// 			productname: product.productname,
-					// 			pushToken: response.data[0].pushToken,
-					// 			message: "One of your items just expired!"
-					// 		};
-					// 		axios
-					// 			.post(
-					// 				`http://${process.env.IP_ADDRESS}:3001/users/push-token`,
-					// 				pushObj
-					// 			)
-					// 			.then(response => {
-					// 				//console.log(response)
-					// 			})
-					// 			.catch(err => {
-					// 				console.log(
-					// 					"We ran into a problem sending the expiration alert to the push notification.\n------------------------"
-					// 				);
-					// 				// console.log(err);
-					// 			});
-					// 	});
+
 					updateProduct({ _id: product._id }, { expiredOrNot: true });
-					// const query = {
-					// 	target: {
-					// 		_id: product._id
-					// 	},
-					// 	update: {
-					// 		expiredOrNot: true
-					// 	}
-					// };
-					// axios
-					// 	.put(`http://${process.env.IP_ADDRESS}:3001/updateProduct`, query)
-					// 	.then(response => {
-					// 		//console.log(response)
-					// 	})
-					// 	.catch(err => {
-					// 		console.log(
-					// 			"We ran into a problem expiring the food.\n------------------------"
-					// 		);
-					// 		// console.log(err);
-					// 	});
+
 					updateUser({ _id: product.owner }, { addExpired: true });
-					// const query2 = {
-					// 	target: {
-					// 		_id: product.owner
-					// 	},
-					// 	update: {
-					// 		addExpired: true
-					// 	}
-					// };
-					// axios
-					// 	.put(`http://${process.env.IP_ADDRESS}:3001/updateUser`, query2)
-					// 	.then(response => {
-					// 		//console.log(response)
-					// 	})
-					// 	.catch(err => {
-					// 		console.log(
-					// 			"We ran into a problem updating the user's expired food.\n------------------------"
-					// 		);
-					// 		// console.log(err);
-					// 	});
 				} else if (product.expiredOrNot === true) {
 					console.log(`The ${product.productname} is expired!!!`);
 				} else {
