c51a597b694fca34f44c17b5b5ba87269cd4ddd5
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-07-23 21:47:36 -0500

    Added index.js file for grocery-list view

---
+++ client/src/components/groceryList/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import {
+	Content,
+	List,
+	ListItem,
+	Text,
+	Icon,
+	Left,
+	Body,
+	Right,
+	Switch,
+	Button,
+	Thumbnail
+} from "native-base";
