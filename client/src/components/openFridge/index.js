ace3e26b6f57b6adb1f473af300b60d49ef7f5dd
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-12 20:28:38 -0500

    Refactored index.js in "openFridge" folder with ESLint

--- client/src/components/openFridge/index.js
+++ client/src/components/openFridge/index.js
@@ -1,82 +1,84 @@
-import React from "react";
-import { StyleSheet, TouchableHighlight, View, Image } from "react-native";
-import { Content, Toast, Icon } from "native-base";
-import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
+import React from 'react';
+import {
+  StyleSheet, TouchableHighlight, View, Image,
+} from 'react-native';
+import { Content, Toast } from 'native-base';
+import { vh } from 'react-native-expo-viewport-units';
 
 const styles = StyleSheet.create({
-	pageDiv: {
-		display: "flex",
-		flex: 1,
-		flexDirection: "column",
-		justifyContent: "flex-start",
-		alignContent: "center",
-		alignItems: "center",
-		backgroundColor: "#EBF5FF"
-	},
-	bannerDiv: {
-		display: "flex",
-		flexDirection: "row",
-		flexWrap: "wrap",
-		justifyContent: "space-around",
-		alignContent: "space-around",
-		alignItems: "flex-start",
-		height: "100%"
-	},
-	banner: {
-		height: "auto",
-		width: "45%"
-	},
-	bannerImg: {
-		height: 125,
-		width: "100%"
-	},
-	disabledBannerImg: {
-		height: 125,
-		width: "100%",
-		opacity: 0.3
-	},
-	grainsImg: {
-		height: 132,
-		width: "100%"
-	},
-	disabledGrainsImg: {
-		height: 132,
-		width: "100%",
-		opacity: 0.3
-	},
-	meatImg: {
-		height: 110,
-		width: "100%"
-	},
-	disabledMeatImg: {
-		height: 110,
-		width: "100%",
-		opacity: 0.3
-	},
-	produceImg: {
-		height: 105,
-		width: "100%"
-	},
-	disabledProduceImg: {
-		height: 105,
-		width: "100%",
-		opacity: 0.3
-	}
+  pageDiv: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EBF5FF',
+  },
+  bannerDiv: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignContent: 'space-around',
+    alignItems: 'flex-start',
+    height: '100%',
+  },
+  banner: {
+    height: 'auto',
+    width: '45%',
+  },
+  bannerImg: {
+    height: 125,
+    width: '100%',
+  },
+  disabledBannerImg: {
+    height: 125,
+    width: '100%',
+    opacity: 0.3,
+  },
+  grainsImg: {
+    height: 132,
+    width: '100%',
+  },
+  disabledGrainsImg: {
+    height: 132,
+    width: '100%',
+    opacity: 0.3,
+  },
+  meatImg: {
+    height: 110,
+    width: '100%',
+  },
+  disabledMeatImg: {
+    height: 110,
+    width: '100%',
+    opacity: 0.3,
+  },
+  produceImg: {
+    height: 105,
+    width: '100%',
+  },
+  disabledProduceImg: {
+    height: 105,
+    width: '100%',
+    opacity: 0.3,
+  },
 });
 
 const noItemsToast = () => {
-	Toast.show({
-		text: `You haven't added any items in this category. Click "New Product" below!`,
-		buttonText: "Okay",
-		position: "bottom",
-		type: "warning",
-		duration: 30000,
-		style: { marginBottom: vh(9) }
-	});
+  Toast.show({
+    text: 'You haven\'t added any items in this category. Click "New Product" below!',
+    buttonText: 'Okay',
+    position: 'bottom',
+    type: 'warning',
+    duration: 30000,
+    style: { marginBottom: vh(9) },
+  });
 };
 
 function Fridge(props) {
-	return (
+  return (
 		<Content contentContainerStyle={styles.pageDiv}>
 			<View style={styles.bannerDiv}>
 				<TouchableHighlight
@@ -87,11 +89,11 @@ function Fridge(props) {
 				>
 					<Image
 						resizeMode="contain"
-						source={require("../../../assets/fridge_images/grains_banner.png")}
+						source={require('../../../assets/fridge_images/grains_banner.png')}
 						style={
 							props.countGrains === 0
-								? styles.disabledGrainsImg
-								: styles.grainsImg
+							  ? styles.disabledGrainsImg
+							  : styles.grainsImg
 						}
 					/>
 				</TouchableHighlight>
@@ -103,11 +105,11 @@ function Fridge(props) {
 				>
 					<Image
 						resizeMode="contain"
-						source={require("../../../assets/fridge_images/dairy_banner.png")}
+						source={require('../../../assets/fridge_images/dairy_banner.png')}
 						style={
 							props.countDairy === 0
-								? styles.disabledBannerImg
-								: styles.bannerImg
+							  ? styles.disabledBannerImg
+							  : styles.bannerImg
 						}
 					/>
 				</TouchableHighlight>
@@ -119,7 +121,7 @@ function Fridge(props) {
 				>
 					<Image
 						resizeMode="contain"
-						source={require("../../../assets/fridge_images/meats_banner.png")}
+						source={require('../../../assets/fridge_images/meats_banner.png')}
 						style={
 							props.countMeats === 0 ? styles.disabledMeatImg : styles.meatImg
 						}
@@ -133,11 +135,11 @@ function Fridge(props) {
 				>
 					<Image
 						resizeMode="contain"
-						source={require("../../../assets/fridge_images/produce_banner.png")}
+						source={require('../../../assets/fridge_images/produce_banner.png')}
 						style={
 							props.countProduce === 0
-								? styles.disabledProduceImg
-								: styles.produceImg
+							  ? styles.disabledProduceImg
+							  : styles.produceImg
 						}
 					/>
 				</TouchableHighlight>
@@ -149,11 +151,11 @@ function Fridge(props) {
 				>
 					<Image
 						resizeMode="contain"
-						source={require("../../../assets/fridge_images/drinks_banner.png")}
+						source={require('../../../assets/fridge_images/drinks_banner.png')}
 						style={
 							props.countDrinks === 0
-								? styles.disabledBannerImg
-								: styles.bannerImg
+							  ? styles.disabledBannerImg
+							  : styles.bannerImg
 						}
 					/>
 				</TouchableHighlight>
@@ -165,17 +167,17 @@ function Fridge(props) {
 				>
 					<Image
 						resizeMode="contain"
-						source={require("../../../assets/fridge_images/misc_banner.png")}
+						source={require('../../../assets/fridge_images/misc_banner.png')}
 						style={
 							props.countMisc === 0
-								? styles.disabledBannerImg
-								: styles.bannerImg
+							  ? styles.disabledBannerImg
+							  : styles.bannerImg
 						}
 					/>
 				</TouchableHighlight>
 			</View>
 		</Content>
-	);
+  );
 }
 
 export default Fridge;
