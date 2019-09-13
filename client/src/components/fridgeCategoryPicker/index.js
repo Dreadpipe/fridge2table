0b06f2c08d56f7a646175bde0549bccc03e05926
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-12 19:45:57 -0500

    Refactored index.js in "fridgeCategory" folder with ESLint

--- client/src/components/fridgeCategoryPicker/index.js
+++ client/src/components/fridgeCategoryPicker/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { Picker, Icon, Item } from "native-base";
+import React from 'react';
+import { Picker, Icon } from 'native-base';
 
 function FridgeCategoryPicker(props) {
 	return (
 		<Picker
-			mode="dropdown"
-			iosIcon={<Icon name="arrow-down" />}
+			mode='dropdown'
+			iosIcon={<Icon name='arrow-down' />}
 			style={props.styles}
-			placeholder="Press to select"
-			placeholderStyle={{ color: "#bfc6ea" }}
-			placeholderIconColor="#007aff"
+			placeholder='Press to select'
+			placeholderStyle={{ color: '#bfc6ea' }}
+			placeholderIconColor='#007aff'
 			selectedValue={props.selectedCategory}
 			onValueChange={props.onCategoryChange}
 		>
-			<Picker.Item label="Dairy" value="Dairy" />
-			<Picker.Item label="Drinks" value="Drinks" />
-			<Picker.Item label="Grains" value="Grains" />
-			<Picker.Item label="Meats" value="Meats" />
-			<Picker.Item label="Misc" value="Misc" />
-			<Picker.Item label="Produce" value="Produce" />
+			<Picker.Item label='Dairy' value='Dairy' />
+			<Picker.Item label='Drinks' value='Drinks' />
+			<Picker.Item label='Grains' value='Grains' />
+			<Picker.Item label='Meats' value='Meats' />
+			<Picker.Item label='Misc' value='Misc' />
+			<Picker.Item label='Produce' value='Produce' />
 		</Picker>
 	);
 }
