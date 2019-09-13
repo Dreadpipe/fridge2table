cc9248843e6d6131b42abe78aea604d5be016f06
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-12 19:21:28 -0500

    Refactored index.js in the "foot" folder with eslint, added new rules to ESLint config file

--- client/src/components/foot/index.js
+++ client/src/components/foot/index.js
@@ -1,22 +1,13 @@
-import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import {
-	Container,
-	Header,
-	Content,
-	Footer,
-	FooterTab,
-	Button,
-	Icon,
-	Text
-} from "native-base";
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
 
 const styles = StyleSheet.create({
 	footer: {
-		backgroundColor: "#193652"
+		backgroundColor: '#193652'
 	},
 	footerTab: {
-		backgroundColor: "#193652"
+		backgroundColor: '#193652'
 	}
 });
 
@@ -25,16 +16,16 @@ function Foot(props) {
 		<Footer style={styles.footer}>
 			<FooterTab style={styles.footerTab}>
 				<Button vertical onPress={props.toAddProductScreenClear}>
-					<Icon name="plus" type="FontAwesome" style={{color: '#EBF5FF'}} />
-					<Text style={{color: '#EBF5FF'}}>New Product</Text>
+					<Icon name="plus" type="FontAwesome" style={{ color: '#EBF5FF' }} />
+					<Text style={{ color: '#EBF5FF' }}>New Product</Text>
 				</Button>
 				<Button vertical onPress={props.toGroceryListScreen}>
-					<Icon name="list" type="FontAwesome" style={{color: '#EBF5FF'}} />
-					<Text style={{color: '#EBF5FF'}}>Grocery List</Text>
+					<Icon name="list" type="FontAwesome" style={{ color: '#EBF5FF' }} />
+					<Text style={{ color: '#EBF5FF' }}>Grocery List</Text>
 				</Button>
 				<Button vertical onPress={props.viewAllProducts}>
-					<Icon name="search" type="FontAwesome" style={{color: '#EBF5FF'}} />
-					<Text style={{color: '#EBF5FF'}}>All Products</Text>
+					<Icon name="search" type="FontAwesome" style={{ color: '#EBF5FF' }} />
+					<Text style={{ color: '#EBF5FF' }}>All Products</Text>
 				</Button>
 			</FooterTab>
 		</Footer>
