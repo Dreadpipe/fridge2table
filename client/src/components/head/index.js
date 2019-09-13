37487d211a3ea2a694c10b651e53e79265a24f56
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-12 20:25:21 -0500

    Refactored index.js in "head" folder with ESLint

--- client/src/components/head/index.js
+++ client/src/components/head/index.js
@@ -1,40 +1,42 @@
-import React, { Component } from "react";
-import { StyleSheet, Image, Platform } from "react-native";
-import { Header, Left, Right, Button, Title, Text } from "native-base";
+import React, { Component } from 'react';
+import { StyleSheet, Image, Platform } from 'react-native';
+import {
+  Header, Left, Right, Button, Text,
+} from 'native-base';
 
 const styles = StyleSheet.create({
-	header: {
-		backgroundColor: "#193652"
-	},
-	logo: {
-		height: "100%",
-		width: Platform.OS === "ios" ? "70%" : "100%"
-	}
+  header: {
+    backgroundColor: '#193652',
+  },
+  logo: {
+    height: '100%',
+    width: Platform.OS === 'ios' ? '70%' : '100%',
+  },
 });
 
 export default class Head extends Component {
-	render() {
-		return (
-			<Header style={styles.header} iosBarStyle='light-content'>
+  render() {
+    return (
+			<Header style={styles.header} iosBarStyle="light-content">
 				<Left>
 					<Image
 						resizeMode="contain"
 						style={styles.logo}
-						source={require("../../../assets/fridge2table_logo.png")}
+						source={require('../../../assets/fridge2table_logo.png')}
 					/>
 				</Left>
 				<Right>
 					<Button hasText transparent onPress={this.props.toFridge}>
-						<Text style={{color: '#EBF5FF'}}>Fridge</Text>
+						<Text style={{ color: '#EBF5FF' }}>Fridge</Text>
 					</Button>
 					<Button hasText transparent onPress={this.props.toFreezer}>
-						<Text style={{color: '#EBF5FF'}}>Freezer</Text>
+						<Text style={{ color: '#EBF5FF' }}>Freezer</Text>
 					</Button>
 					<Button hasText transparent onPress={this.props.toPantry}>
-						<Text style={{color: '#EBF5FF'}}>Pantry</Text>
+						<Text style={{ color: '#EBF5FF' }}>Pantry</Text>
 					</Button>
 				</Right>
 			</Header>
-		);
-	}
+    );
+  }
 }
