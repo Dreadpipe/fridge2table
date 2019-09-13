aa9918e4d89f5b66555a3701c3faf68590632195
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-12 19:45:01 -0500

    Refactored index.js in "freezerCategories" folder with ESLint

--- client/src/components/freezerCategoryPicker/index.js
+++ client/src/components/freezerCategoryPicker/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Picker, Icon, Item } from "native-base";
+import React from 'react';
+import { Picker, Icon } from 'native-base';
 
 function FreezerCategoryPicker(props) {
 	return (
 		<Picker
-			mode="dropdown"
-			iosIcon={<Icon name="arrow-down" />}
+			mode='dropdown'
+			iosIcon={<Icon name='arrow-down' />}
 			style={props.styles}
-			placeholder="Press to select"
-			placeholderStyle={{ color: "#bfc6ea" }}
-			placeholderIconColor="#007aff"
-			selectedValue={"Fridge"}
+			placeholder='Press to select'
+			placeholderStyle={{ color: '#bfc6ea' }}
+			placeholderIconColor='#007aff'
+			selectedValue={'Fridge'}
 			selectedValue={props.selectedCategory}
 			onValueChange={props.onCategoryChange.bind(this)}
 		>
-			<Picker.Item label="Meats" value="Meats" />
-			<Picker.Item label="Misc" value="Misc" />
-            <Picker.Item label="Produce" value="Produce" />
+			<Picker.Item label='Meats' value='Meats' />
+			<Picker.Item label='Misc' value='Misc' />
+			<Picker.Item label='Produce' value='Produce' />
 		</Picker>
 	);
 }
